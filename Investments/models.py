980f3e1468616f6587d2d83ad0cfc303c4d47637
Miecznikk <yohkun@wp.pl>
2023-10-18 18:55:53 +0200

    updated apartment model

--- Investments/models.py
+++ Investments/models.py
@@ -1,11 +1,16 @@
+import logging
+
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
-from django.db.models import Min,Max
+from django.db.models import Min, Max
 from Users.models import InvestorUser
+from django.core.exceptions import FieldDoesNotExist
 
 
 class Investment(models.Model):
+    logger = logging.getLogger(__name__)
+
     name = models.CharField(max_length=60, null=False)
     image = models.ImageField(upload_to='images/investments')
     resized_image = models.ImageField(upload_to='images/investments/resized', blank=True, null=True)
@@ -19,13 +24,17 @@ class Investment(models.Model):
     def get_reversed_url(self):
         return reverse('investments:investment_detail', args=[str(self.id)])
 
-    def get_area_range(self):
-        apartments = Apartment.objects.filter(investment=self).aggregate(min_area=Min('area'),
-                                                                         max_area=Max('area'))
-        min_area = apartments['min_area']
-        max_area = apartments['max_area']
-
-        return f"{min_area}-{max_area}"
+    def get_number_parameter_range(self, parameter):
+        try:
+            field = Apartment._meta.get_field(parameter)
+            if isinstance(field, (models.IntegerField, models.PositiveIntegerField, models.FloatField)):
+                apartments = Apartment.objects.filter(investment=self).aggregate(min_value=Min(parameter),
+                                                                                 max_value=Max(parameter))
+                return f"{apartments['min_value']} - {apartments['max_value']}"
+            else:
+                return ""
+        except FieldDoesNotExist as e:
+            self.logger.error(str(e))
 
 
 class Apartment(models.Model):
@@ -34,12 +43,23 @@ class Apartment(models.Model):
     area = models.FloatField(validators=[
         MinValueValidator(10.0, message="Value must be greater than 10 square meters"),
         MaxValueValidator(300.0, message="Value cannot be greater than 300 square meters")
-    ])
+    ], null=False)
     rooms = models.IntegerField(validators=[
         MinValueValidator(1, message="Value must be greater or exual 1"),
         MaxValueValidator(10, message="Value cannot be greater than 10")
-    ])
-    investment = models.ForeignKey(Investment,on_delete=models.CASCADE, null=False)
+    ], null=False)
+    price = models.PositiveIntegerField(validators=[
+        MinValueValidator(1, message="Value must be greater than 0"),
+        MaxValueValidator(100_000_000, message="Value cannot exceed 100 million")],
+        null=False)
+    investment = models.ForeignKey(Investment, on_delete=models.CASCADE, null=False)
+    floor = models.PositiveIntegerField(validators=[MaxValueValidator(20)])
+    has_balcony = models.BooleanField(null=False)
+    has_garden = models.BooleanField(null=False)
+    has_basement = models.BooleanField(null=False)
+    has_garage = models.BooleanField(null=False)
+    has_AC = models.BooleanField(null=False)
+    two_floor_apartment = models.BooleanField(null=False)
 
     def __str__(self):
         return f"{self.investment.name}: {self.get_full_address()}"
@@ -47,6 +67,10 @@ class Apartment(models.Model):
     def get_full_address(self):
         return (f"{self.investment.street} {self.building_number}"
                 f"{"/" + self.apartment_number if self.apartment_number else ""}")
+
+    def get_price_per_square_meter(self):
+        return f"{round(self.price / self.area, 2)}"
+
 
 class ApartmentImage(models.Model):
     image = models.ImageField(upload_to='images/apartments')
